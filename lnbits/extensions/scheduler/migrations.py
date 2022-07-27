4641e00dacd460b9bcffb1243b56b11dab59f5c5
ngutech21 <ngutech21@pm.me>
2022-07-27 08:38:19 +0200

    cleanup migrations

--- lnbits/extensions/scheduler/migrations.py
+++ lnbits/extensions/scheduler/migrations.py
@@ -1,12 +1,8 @@
-from loguru import logger
-
-
 async def m001_initial(db):
-    logger.info("migrate db")
-    print("aaaaaaaaacjkjjkjkjkjkjkjkjkjkjk")
+    print("migrate scheduler DB")
     await db.execute(
         f"""
-       CREATE TABLE scheduler.job_config (
+         CREATE TABLE scheduler.job_config (
            id TEXT PRIMARY KEY,
            user_name TEXT NOT NULL,
            wallet TEXT NOT NULL,
